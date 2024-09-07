2b928b2a2773638cd428c7217908928cdb457333
Maksim1803 <maks1803@yandex.ru>
2024-09-07 19:08:53 +0300

    First commit.

---
+++ src/test/kotlin/Kingdom_3KtTest.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.*
+
+class Kingdom_3KtTest {
+
+    @org.junit.jupiter.api.Test
+    fun getWorkerTaxCollector3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getBuilderTaxCollector3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getFarmerTaxCollector3() {
+    }
+}
